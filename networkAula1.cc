9a31f2934b13e4c88fa62c3852f162111f5295c4
Andreia Alexandre Felix <110141565+AndreiaAF16@users.noreply.github.com>
2024-03-13 20:50:15 -0300

    networkAula1.cc

--- networkAula1.cc
+++ networkAula1.cc
@@ -6,19 +6,18 @@ using namespace omnetpp;
 class Node: public cSimpleModule{
 protected:
     virtual void initialize() override;
-    virtual void handleMessage(Message *msg) override;
+    virtual void handleMessage(cMessage *msg) override;
 };
 
 Define_Module(Node);
 
 void Node::initialize(){
     if(strcmp("NodeA", getName()) == 0){
-        std::cout << "Olá mundo!" << endl;
    	 cMessage *msg = new cMessage("MSG TIC TOC!");
    	 send(msg, "out");
     }
 }
 
-void Node::handleMessage(cMesssage *msg){
+void Node::handleMessage(cMessage *msg){
     send(msg, "out");
 }
