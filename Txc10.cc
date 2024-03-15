045000198dbc6411d403e7376cf7047bb23ec4e7
Andreia Alexandre Felix <110141565+AndreiaAF16@users.noreply.github.com>
2024-03-14 23:01:04 -0300

    Create Txc10.cc

---
+++ Txc10.cc
@@ -0,0 +1,52 @@
+#include <omnetpp.h>
+#include <Txc10.h>
+
+Define_Module(Txc10);
+
+Txc10::Txc10() {
+
+}
+
+Txc10::~Txc10() {
+
+}
+
+void Txc10::initialize() {
+    if (getIndex() == 0) {
+        // Boot the process scheduling the initial message as a self-message.
+        char msgname[20];
+        sprintf(msgname, "tic-%d", getIndex());
+        cMessage *msg = new cMessage(msgname);
+        scheduleAt(0.0, msg);
+    }
+}
+
+void Txc10::handleMessage(cMessage *msg) {
+    if (getIndex() == 3) {
+        // Message arrived.
+        EV << "Message " << msg << " arrived.\n";
+        delete msg;
+    } else {
+        // We need to forward the message.
+        forwardMessage(msg);
+    }
+}
+
+void Txc10::forwardMessage(cMessage *msg) {
+    // In this example, we just pick a random gate to send it on.
+    // We draw a random number between 0 and the size of gate `out[]'.
+    int n = gateSize("out");
+    int k = intuniform(0, n - 1);
+//    if (!(msg->isSelfMessage())) {
+//        EV << "Message " << msg << " is a not a self message.";
+//        cGate *gate = msg->getArrivalGate()->getIndex();
+//        EV << "Message arrived at gate index " << arrival << endl;
+//        while (k == arrival) {
+//            k = intuniform(0,n-1);
+//            EV << "Chose forward gate index " << k << endl;
+//        }
+//    }
+
+    EV << "Forwarding message " << msg << " on port out[" << k << "]\n";
+    send(msg, "out", k);
+}
